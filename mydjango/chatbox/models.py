26dd7855bcbe728d19fbc5c46ab3e60c584a0c85
s1leviathan <28671497+s1leviathan@users.noreply.github.com>
2023-12-15 11:53:59 +0200

    created user model

--- mydjango/chatbox/models.py
+++ mydjango/chatbox/models.py
@@ -1,2 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.user.username
