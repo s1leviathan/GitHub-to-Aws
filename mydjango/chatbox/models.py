e68b949504727877fc4a5d2955967061d845d19e
s1leviathan <28671497+s1leviathan@users.noreply.github.com>
2023-12-15 12:14:27 +0200

    message model

--- mydjango/chatbox/models.py
+++ mydjango/chatbox/models.py
@@ -7,3 +7,10 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='sent_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
