e6c5c1117e4961248ec15851796d0dd7489dbc46
s1leviathan <28671497+s1leviathan@users.noreply.github.com>
2023-12-15 12:45:44 +0200

    users login

--- mydjango/chatbox/views.py
+++ mydjango/chatbox/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('chatbox')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'chatbox/login.html', {'form': form})
