c4d84d13b420cd6155467f5f43322c296a96391b
s1leviathan <28671497+s1leviathan@users.noreply.github.com>
2023-12-17 21:02:20 +0200

    home urls

--- mydjango/mydjango/urls.py
+++ mydjango/mydjango/urls.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('chatbox.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),  # Include authentication URLs
-    path('chat/', include('chatbox.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('chat/', include('chat.urls')),  
 ]
+
