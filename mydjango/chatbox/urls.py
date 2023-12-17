3cc9428a7754527dbfaafe474787211689ede038
s1leviathan <28671497+s1leviathan@users.noreply.github.com>
2023-12-17 21:16:17 +0200

    changed the order

--- mydjango/chatbox/urls.py
+++ mydjango/chatbox/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from .views import user_login, chat_view, view_conversation, chat_list, home_view
 
 urlpatterns = [
-    path('', home_view, name='home'),
+    
     path('login/', user_login, name='user_login'),
     path('chat/', chat_view, name='chat'),
     path('conversation/<int:conversation_id>/', view_conversation, name='view_conversation'),
     path('chat-list/', chat_list, name='chat_list'),
+    path('', home_view, name='home'),
 ]
--- mydjango/mydjango/urls.py
+++ mydjango/mydjango/urls.py
@@ -4,9 +4,10 @@ from django.urls import path, include
 from chatbox.views import home_view
 
 urlpatterns = [
-    path('', home_view, name='home'),
+    
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('chat/', include('chat.urls')),  
+    path('', home_view, name='home'),
 ]
 
