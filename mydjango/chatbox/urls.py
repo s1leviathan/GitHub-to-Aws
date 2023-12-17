0e39f4b4ee8433ef925701c469e4d0210c6616be
s1leviathan <28671497+s1leviathan@users.noreply.github.com>
2023-12-17 21:13:25 +0200

    def home view

--- mydjango/chatbox/urls.py
+++ mydjango/chatbox/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import user_login, chat_view, view_conversation, chat_list
+from .views import user_login, chat_view, view_conversation, chat_list, home_view
 
 urlpatterns = [
     path('', home_view, name='home'),
--- mydjango/mydjango/urls.py
+++ mydjango/mydjango/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from chatbox.views import home_view
 
 urlpatterns = [
-    path('', include('chatbox.urls')),
+    path('', home_view, name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('chat/', include('chat.urls')),  
